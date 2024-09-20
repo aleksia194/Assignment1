7f12a62b0ff4ce044f44b2375a057fe0d59bd83d
Aleksandr Babikov <st135743@student.spbu.ru>
2024-09-20 02:24:20 +0000

    Контрольный коммит
    добавление обработки исключения и проверка всех файлов на работоспособность

--- hello.cpp
+++ hello.cpp
@@ -4,8 +4,9 @@
 void hello() {
     std::string input;
     while (true) {
-        std::cout << "read your name: ";
+        std::cout << "(exit) read your name: ";
         std::getline(std::cin, input);
+        if (input == "exit"){break;}
         
         std::cout << "Hello, " << input << "!" << std::endl;
         
